dc32a3396cf761c4be3bfb713b6ef24432c4bc6a
MohamedThowhith <mdthowhith.11@gmail.com>
2026-05-03 13:49:00 +0530

    added cors support

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+// Enable CORS (IMPORTANT for frontend connection)
+app.use(cors());
+
+// Routes
 app.get("/", (req, res) => {
     res.send("DevOps Project Running 🚀");
 });
@@ -9,6 +15,7 @@ app.get("/health", (req, res) => {
     res.json({ status: "OK" });
 });
 
+// Start server
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
