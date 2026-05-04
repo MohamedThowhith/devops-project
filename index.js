51042a893d0c37ad4318eac4c65a918feb4ba66e
MohamedThowhith <mdthowhith.11@gmail.com>
2026-05-04 20:53:10 +0530

    added database

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+const { Pool } = require("pg");
 
 const app = express();
-
-// Enable CORS (IMPORTANT for frontend connection)
 app.use(cors());
+app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => {
-    res.send("DevOps Project Running 🚀");
+// Use Render environment variable
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
-app.get("/health", (req, res) => {
-    res.json({ status: "OK" });
+// Create table
+app.get("/init", async (req, res) => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS expenses (
+            id SERIAL PRIMARY KEY,
+            title TEXT,
+            amount INT
+        )
+    `);
+    res.send("Table created");
 });
 
-// Start server
+// Add expense
+app.post("/add-expense", async (req, res) => {
+    const { title, amount } = req.body;
+    await pool.query(
+        "INSERT INTO expenses (title, amount) VALUES ($1, $2)",
+        [title, amount]
+    );
+    res.send("Expense added");
+});
+
+// Get all expenses
+app.get("/expenses", async (req, res) => {
+    const result = await pool.query("SELECT * FROM expenses ORDER BY id DESC");
+    res.json(result.rows);
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
